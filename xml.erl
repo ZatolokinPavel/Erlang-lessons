81c92ba89386b807893bc0c936f6f52c14d1895f
Затолокин Павел <ZatolokinPavel@yandex.ru>
2014-09-07 15:21:17 +0300

    Добавил обработку атрибутов. Пример, как теперь работает модуль, я пока написал в файле README.md. Конечно же, модуль далёк от соответствия всяким стандартам XML. Модуль не проверяет, чтобы закрывающий тег совпадал с открывающим. А ещё, вложенные теги, идущие подряд на одном уровне вложенности, сохраняются в обратном порядке.

--- xml.erl
+++ xml.erl
@@ -1,30 +1,48 @@
 -module(xml).
--export([decode/1]).
+-export([decode/1, tag/2]).
 
-decode(String) -> decode(String, {<<>>, [], <<>>}).
+decode(String) -> decode(String, {[], [], <<>>}).
 
 decode(<<"</", T/binary>>, {Tag, Next, Cont}) ->
-    {Bin, Tag} = tag(T, <<>>),
+    {Bin, _} = tag(T, [<<>>]),
     case{Bin == <<>>} of
         {false} -> {Bin, {Tag, Next, Cont}};
         {true}  -> {Tag, Next, Cont}
     end;
-decode(<<"<", T/binary>>, {<<>>, [], Cont}) ->
-    {Bin, Tag} = tag(T, <<>>),
-	decode(Bin, {Tag, [], Cont});
+decode(<<"<", T/binary>>, {[], [], Cont}) ->
+    {Bin, Tag} = tag(T, [<<>>]),
+    decode(Bin, {Tag, [], Cont});
 decode(<<"<", T/binary>>, {Tag, Next, Cont}) ->
-    {TT, NextLevel} = decode(<<"<", T/binary>>, {<<>>, [], <<>>}),
+    {TT, NextLevel} = decode(<<"<", T/binary>>, {[], [], <<>>}),
     decode(TT, {Tag, [NextLevel|Next], Cont});
 decode(Bin, {Tag, Next, _}) ->
     {T, Cont} = content(Bin, <<>>),
-	decode(T, {Tag, Next, Cont}).
+    decode(T, {Tag, Next, Cont}).
 
 tag(<<">", T/binary>>, Acc) ->
+    {T, reverse(Acc)};
+tag(<<" ", T/binary>>, Acc) ->
+    {Bin, Att} = attribute(T, {<<>>, <<>>}),
+    tag(Bin, [Att | Acc]);
+tag(<<H:1/binary, T/binary>>, [Acc]) ->
+    tag(T, [<<Acc/binary, H/binary>>]).
+
+attribute(<<"=\"", H:1/binary, T/binary>>, {Name, <<>>}) ->
+    attribute(T, {Name, H});
+attribute(<<"\"", T/binary>>, Acc) ->
     {T, Acc};
-tag(<<H:1/binary, T/binary>>, Acc) ->
-    tag(T, <<Acc/binary, H/binary>>).
+attribute(<<H:1/binary, T/binary>>, {Name, <<>>}) ->
+    attribute(T, {<<Name/binary, H/binary>>, <<>>});
+attribute(<<H:1/binary, T/binary>>, {Name, Value}) ->
+    attribute(T, {Name, <<Value/binary, H/binary>>}).
 
 content(<<"<", T/binary>>, Acc) ->
     {<<"<", T/binary>>, Acc};
 content(<<H:1/binary, T/binary>>, Acc) ->
     content(T, <<Acc/binary, H/binary>>).
+
+
+reverse(List1) -> reverse(List1,[]).
+reverse([], List2) -> List2;
+reverse([H|T], List2) -> reverse(T, [H|List2]).
+
