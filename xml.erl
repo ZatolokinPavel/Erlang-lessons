d923b74e8dc2cbda47c01619a3f29b193fa23bc3
Затолокин Павел <ZatolokinPavel@yandex.ru>
2014-09-08 20:37:37 +0300

    Переделал обработку атрибутов и вложенных уровней согласно заданию. Пример, что давать модулю и что он выдаст, я пока написал в файле README.md. Модуль всё ещё не делает очень многих проверок правильности входного кода, поэтому его легко завалить некорректным заданием.

--- xml.erl
+++ xml.erl
@@ -1,32 +1,40 @@
 -module(xml).
--export([decode/1, tag/2]).
+-export([decode/1]).
 
-decode(String) -> decode(String, {[], [], <<>>}).
+% Первичное преобразование и самая простая проверка
+decode(String) ->
+    case{String} of
+        {<<>>}  -> error;
+        {_}     -> decode(String, {<<>>, [], []})
+    end.
 
-decode(<<"</", T/binary>>, {Tag, Next, Cont}) ->
-    {Bin, _} = tag(T, [<<>>]),
+
+% Основное тело программы, где определяются теги и контент
+decode(<<"</", T/binary>>, {Tag, Attrib, Content}) ->
+    {Bin, {Tag, []}} = tag(T, {<<>>, []}),
     case{Bin == <<>>} of
-        {false} -> {Bin, {Tag, Next, Cont}};
-        {true}  -> {Tag, Next, Cont}
+        {false} -> {Bin, {Tag, Attrib, Content}};
+        {true}  -> {Tag, Attrib, Content}
     end;
-decode(<<"<", T/binary>>, {[], [], Cont}) ->
-    {Bin, Tag} = tag(T, [<<>>]),
-    decode(Bin, {Tag, [], Cont});
-decode(<<"<", T/binary>>, {Tag, Next, Cont}) ->
-    {TT, NextLevel} = decode(<<"<", T/binary>>, {[], [], <<>>}),
-    decode(TT, {Tag, [NextLevel|Next], Cont});
-decode(Bin, {Tag, Next, _}) ->
-    {T, Cont} = content(Bin, <<>>),
-    decode(T, {Tag, Next, Cont}).
+decode(<<"<", T/binary>>, {<<>>, [], Cont}) ->
+    {Bin, {Tag, Att}} = tag(T, {<<>>, []}),
+    decode(Bin, {Tag, Att, Cont});
+decode(Bin, {Tag, Attrib, []}) ->
+    {T, Cont} = content(Bin, <<>>, []),
+    decode(T, {Tag, Attrib, Cont}).
 
-tag(<<">", T/binary>>, Acc) ->
-    {T, reverse(Acc)};
-tag(<<" ", T/binary>>, Acc) ->
+
+% Обработка тегов
+tag(<<">", T/binary>>, {Tag, Att}) ->
+    {T, {Tag, reverse(Att)}};
+tag(<<" ", T/binary>>, {Tag, Acc}) ->
     {Bin, Att} = attribute(T, {<<>>, <<>>}),
-    tag(Bin, [Att | Acc]);
-tag(<<H:1/binary, T/binary>>, [Acc]) ->
-    tag(T, [<<Acc/binary, H/binary>>]).
+    tag(Bin, {Tag, [Att | Acc]});
+tag(<<H:1/binary, T/binary>>, {Tag, []}) ->
+    tag(T, {<<Tag/binary, H/binary>>, []}).
 
+
+% Обработка атрибутов тегов
 attribute(<<"=\"", H:1/binary, T/binary>>, {Name, <<>>}) ->
     attribute(T, {Name, H});
 attribute(<<"\"", T/binary>>, Acc) ->
@@ -36,12 +44,20 @@ attribute(<<H:1/binary, T/binary>>, {Name, <<>>}) ->
 attribute(<<H:1/binary, T/binary>>, {Name, Value}) ->
     attribute(T, {Name, <<Value/binary, H/binary>>}).
 
-content(<<"<", T/binary>>, Acc) ->
-    {<<"<", T/binary>>, Acc};
-content(<<H:1/binary, T/binary>>, Acc) ->
-    content(T, <<Acc/binary, H/binary>>).
+
+% Обработка контента и перенаправление вложений в основную часть
+content(<<"</", T/binary>>, <<>>, Cont) ->
+    {<<"</", T/binary>>, reverse(Cont)};
+content(<<"<", T/binary>>, <<>>, Cont) ->
+    {Bin, NextLevel} = decode(<<"<", T/binary>>),
+    content(Bin, <<>>, [NextLevel|Cont]);
+content(<<"<", T/binary>>, Acc, Cont) ->
+    content(<<"<", T/binary>>, <<>>, [Acc|Cont]);
+content(<<H:1/binary, T/binary>>, Acc, Cont) ->
+    content(T, <<Acc/binary, H/binary>>, Cont).
 
 
+% Помогает переворачивать списки
 reverse(List1) -> reverse(List1,[]).
 reverse([], List2) -> List2;
 reverse([H|T], List2) -> reverse(T, [H|List2]).
